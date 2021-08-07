dc909744664af58ce18c371e32732922ffc879d8
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2021-08-07 19:39:55 -0400

    added Node.java

---
+++ src/Node.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+
+// I need Node for two reasons: the backtracking and generation tracking (for the a* algorithm, a square's score is distance from end + generation)
+public class Node {
+
+}
