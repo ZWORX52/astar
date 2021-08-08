ae76f00817c8ebadf626c3fcb60bd7988fb98b5d
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2021-08-08 11:04:39 -0400

    doing things on Node.java and Main.java

--- src/Node.java
+++ src/Node.java
@@ -3,5 +3,24 @@ import java.util.*;
 
 // I need Node for two reasons: the backtracking and generation tracking (for the a* algorithm, a square's score is distance from end + generation)
 public class Node {
+    public int x;
+    public int y;
+    public int cost;
+    private final int generation;
+    private Node parent;
 
+    Node(int x, int y, Node parent) {
+        this.x = x;
+        this.y = y;
+        this.parent = parent;
+        this.generation = parent.generation + 1;
+    }
+
+    public void calculate_cost(int endX, int endY) {
+        // Cost is the generation + (x distance to end ^ 2 + y distance to end ^ 2)
+        int xDistance = Math.abs(this.x - endX);
+        int yDistance = Math.abs(this.y - endY);
+        int distance = xDistance * xDistance + yDistance * yDistance;
+        this.cost = distance + this.generation;
+    }
 }
